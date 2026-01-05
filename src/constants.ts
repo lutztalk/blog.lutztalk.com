41fb53d361c4fd5ccccc1aaaa01b3edae2ed0c57
Austin Lutz <durpnet@turtlebook-air.local>
2026-01-05 16:53:10 -0500

    Update dark mode to black, redirect about page, update social handles
    - Change dark mode background from navy blue to pure black (#000000)
    - Update about page to redirect to lutztalk.com
    - Update social handles: @lutztalk on X, @ayegel on LinkedIn
    - Remove GitHub and Mail social links

--- src/constants.ts
+++ src/constants.ts
@@ -18,28 +18,16 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
-    name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitHub`,
-    icon: IconGitHub,
-  },
-  {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/lutztalk",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/ayegel/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    icon: IconMail,
   },
 ] as const;
 
